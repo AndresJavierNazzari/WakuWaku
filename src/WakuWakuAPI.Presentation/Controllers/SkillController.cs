edfa2dd3585ad31a52161037737648064c64de64
Andres J. Nazzari <andres.nazzari@jala.university>
2023-12-20 18:07:15 -0300

    Create SkillController

---
+++ src/WakuWakuAPI.Presentation/Controllers/SkillController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WakuWakuAPI.Presentation.Controllers {
+    public class SkillController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
