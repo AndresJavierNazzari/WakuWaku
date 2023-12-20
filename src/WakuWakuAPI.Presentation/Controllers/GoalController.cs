55aa1c1f17d8a50ce962a795585decd900998588
Andres J. Nazzari <andres.nazzari@jala.university>
2023-12-20 18:07:05 -0300

    Create GoalController

---
+++ src/WakuWakuAPI.Presentation/Controllers/GoalController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WakuWakuAPI.Presentation.Controllers {
+    public class GoalController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
