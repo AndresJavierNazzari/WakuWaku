eb110138269e3762c9f81923f95bf9908fb51e0f
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:06:58 -0300

    Create DTOs GoalForCreation and GoalForUpdate

---
+++ src/WakuWakuAPI.Domain/DTOs/GoalForCreation.cs
@@ -0,0 +1,8 @@
+﻿namespace WakuWakuAPI.Domain.DTOs;
+public class GoalForCreation {
+    public string Description { get; set; }
+    public DateTime DateOfCreation { get; set; }
+    public DateTime Deadline { get; set; }
+    public String Status { get; set; }
+}
+
---
+++ src/WakuWakuAPI.Domain/DTOs/GoalForUpdate.cs
@@ -0,0 +1,7 @@
+﻿namespace WakuWakuAPI.Domain.DTOs;
+public class GoalForUpdate {
+    public string Description { get; set; }
+    public DateTime DateOfCreation { get; set; }
+    public DateTime Deadline { get; set; }
+    public String Status { get; set; }
+}
