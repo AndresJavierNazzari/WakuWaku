1a919ff738b07d6f58bb76d105417d45884476a9
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:08:51 -0300

    Create ICategoryService

---
+++ src/WakuWakuAPI.Application/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using WakuWakuAPI.Domain.Models;
+using WakuWakuAPI.Domain.DTOs;
+
+namespace WakuWakuAPI.Application.Services.Interfaces;
+public interface ICategoryService {
+    IEnumerable<Category> GetCategories(int page, int pageSize, string filter);
+    Category GetCategoryById(int id);
+    Category AddCategory(CategoryForCreation categoryForCreation);
+    Category UpdateCategory(int id, CategoryForUpdate categoryForUpdate);
+    Category DeleteCategoryById(int id);
+}
+
