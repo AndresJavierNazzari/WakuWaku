5ae81d0a032b3f06fc76c68309fd0c8e8cb2bf32
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:08:59 -0300

    Create IGoalService

---
+++ src/WakuWakuAPI.Application/Services/Interfaces/IGoalService.cs
@@ -0,0 +1,12 @@
+﻿
+using WakuWakuAPI.Domain.Models;
+using WakuWakuAPI.Domain.DTOs;
+
+namespace WakuWakuAPI.Application.Services.Interfaces;
+public interface IGoalService {
+    IEnumerable<Goal> GetGoals(int page, int pageSize, string filter);
+    Goal GetGoalById(int id);
+    Goal AddGoal(GoalForCreation goalForCreation);
+    Goal UpdateGoal(int goalId, GoalForUpdate goalForUpdate);
+    Goal DeleteGoalById(int id);
+}
