19deed805fdc8cf432138d36e1be8f345d3c1610
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:09:37 -0300

    Create EmptyIdException

---
+++ src/WakuWakuAPI.Application/CrossCutting/EmptyIdException.cs
@@ -0,0 +1,12 @@
+﻿namespace WakuWakuAPI.Application.CrossCutting;
+public class EmptyIdException : ArgumentException {
+    public EmptyIdException(string message) : base(message) {
+    }
+
+    public static void ThrowIfEmpty(
+        int id, string? message = null, string? elementName = null) {
+        if(id == 0) {
+            throw new EmptyIdException(message ?? $"{elementName} Id cannot be empty");
+        }
+    }
+}
