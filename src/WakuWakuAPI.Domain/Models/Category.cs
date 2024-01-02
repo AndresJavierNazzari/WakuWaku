c4b730c80670d551c7662f7ea5922106ea2b7596
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:06:13 -0300

    Create Category Model

---
+++ src/WakuWakuAPI.Domain/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace WakuWakuAPI.Domain.Models;
+public class Category {
+    private static int lastId = 0;
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Category(string name, string description) {
+        this.Id = ++lastId;
+        this.Name = name;
+        this.Description = description;
+    }
+}
