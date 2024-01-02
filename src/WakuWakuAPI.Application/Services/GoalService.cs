d7da0e714016574ffdde7ac9d23bb92330db8e66
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:09:21 -0300

    Create GoalService

---
+++ src/WakuWakuAPI.Application/Services/GoalService.cs
@@ -0,0 +1,51 @@
+﻿
+using WakuWakuAPI.Domain.Models;
+using WakuWakuAPI.Domain.DTOs;
+using WakuWakuAPI.Infraestructure.Repositories.Interfaces;
+using WakuWakuAPI.Application.Services.Interfaces;
+
+namespace WakuWakuAPI.Application.Services;
+public class GoalService : IGoalService {
+    private readonly IGoalRepository _goalRepository;
+
+    public GoalService(IGoalRepository goalRepository) {
+        _goalRepository = goalRepository;
+    }
+
+    public IEnumerable<Goal> GetGoals(int page, int pageSize, string filter) {
+        var goalList = _goalRepository.GetGoals();
+        var paginatedGoals = goalList.Skip((page - 1) * pageSize).Take(pageSize);
+
+        if(!string.IsNullOrEmpty(filter)) {
+            paginatedGoals =
+                paginatedGoals.Where(c => c.Description.Contains(filter, StringComparison.OrdinalIgnoreCase));
+        }
+
+        return paginatedGoals;
+    }
+
+    public Goal GetGoalById(int id) {
+        Goal goal = _goalRepository.GetGoalById(id);
+
+        return goal;
+    }
+
+    public Goal AddGoal(GoalForCreation goalForCreation) {
+        Goal createdGoal = _goalRepository.AddGoal(goalForCreation);
+
+        return createdGoal;
+    }
+
+    public Goal UpdateGoal(int id, GoalForUpdate goalForUpdate) {
+        Goal updatedGoal = _goalRepository.UpdateGoal(id, goalForUpdate);
+
+        return updatedGoal;
+    }
+
+    public Goal DeleteGoalById(int id) {
+        Goal goal = _goalRepository.DeleteGoalById(id);
+        //NotFoundException.ThrowIfNull(goal);
+
+        return goal;
+    }
+}
