e396a6d9771d0ecff19fb0a0f329b6e6bd5762b3
Andres J. Nazzari <andres.nazzari@jala.university>
2024-01-02 18:09:46 -0300

    Create NotFoundException

---
+++ src/WakuWakuAPI.Application/CrossCutting/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace WakuWakuAPI.Application.CrossCutting;
+public class NotFoundException : ArgumentException {
+    public NotFoundException(string? message) : base(message) { }
+
+    public static void ThrowIfNull<T>(T? param, string? message = null) {
+        if(param is null) {
+            throw new NotFoundException(message ?? "Resource was not found");
+        }
+    }
+}
+
