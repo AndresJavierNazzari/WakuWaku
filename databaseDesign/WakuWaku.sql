5ac8d928de5f8eedc3c77e23d111755a2edb1407
Andres Nazzari <andres.nazzari@jala.university>
2024-01-31 19:17:43 -0300

    Refactor database schema and add foreign key constraints
    This commit refactors the database schema in the WakuWaku.sql file. It adds primary key constraints with auto-incrementing IDs to the Categories, Goals, GoalsSkills, Status, Skills, Users, and UserData tables. It also adds foreign key constraints to establish relationships between tables. Additionally, it adds the Dificulty table with a foreign key constraint on the Goals table. These changes improve data integrity and maintainability of the database design.

--- databaseDesign/WakuWaku.sql
+++ databaseDesign/WakuWaku.sql
@@ -1,69 +1,83 @@
 CREATE TABLE [Categories] (
-  [Id] integer PRIMARY KEY,
-  [Name] varchar(200),
-  [Description] varchar(200)
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [Name] varchar(50) NOT NULL,
+  [Description] varchar(200) NOT NULL
 )
 GO
 
 CREATE TABLE [Goals] (
-  [Id] integer PRIMARY KEY,
-  [Description] varchar(200),
-  [DateOfCreation] datetime,
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [Description] varchar(200) NOT NULL,
   [Deadline] datetime,
-  [StatusId] integer
+  [StatusId] integer NOT NULL,
+  [DificultyId] integer,
+  [CreatedAt] datetime NOT NULL DEFAULT (getDate()),
+  [UpdatedAt] datetime
 )
 GO
 
 CREATE TABLE [GoalsSkills] (
-  [Id] integer PRIMARY KEY,
-  [GoalId] integer,
-  [SkillId] integer
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [GoalId] integer NOT NULL,
+  [SkillId] integer NOT NULL
 )
 GO
 
 CREATE TABLE [Status] (
-  [Id] integer PRIMARY KEY,
-  [Description] varchar(200)
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [Description] nvarchar(255) NOT NULL CHECK ([Description] IN ('Completed', 'In Progress')) NOT NULL
 )
 GO
 
 CREATE TABLE [Skills] (
-  [Id] integer PRIMARY KEY,
-  [Name] varchar(200),
-  [CategoryId] integer
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [Name] varchar(50) NOT NULL,
+  [Description] varchar(200) NOT NULL,
+  [CategoryId] integer NOT NULL,
+  [CreatedAt] datetime NOT NULL DEFAULT (getDate()),
+  [UpdatedAt] datetime
 )
 GO
 
 CREATE TABLE [Users] (
-  [Id] integer PRIMARY KEY,
-  [Email] nvarchar(200) UNIQUE,
-  [FirstName] nvarchar(200),
-  [LastName] nvarchar(200),
-  [Password] nvarchar(200)
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [GoalId] integer NOT NULL,
+  [FirstName] nvarchar(50) NOT NULL,
+  [LastName] nvarchar(50) NOT NULL,
+  [UserDataId] integer NOT NULL
 )
 GO
 
-CREATE TABLE [UsersGoals] (
-  [Id] integer PRIMARY KEY,
-  [GoalId] integer,
-  [UserId] integer
+CREATE TABLE [UserData] (
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [Email] nvarchar(100) UNIQUE NOT NULL,
+  [Password] nvarchar(200) NOT NULL
 )
 GO
 
-ALTER TABLE [UsersGoals] ADD FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id])
-GO
-
-ALTER TABLE [UsersGoals] ADD FOREIGN KEY ([GoalId]) REFERENCES [Goals] ([Id])
+CREATE TABLE [Dificulty] (
+  [Id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [Description] nvarchar(255) NOT NULL CHECK ([Description] IN ('Very Easy', 'Easy', 'Normal', 'Hard', 'Very Hard')) NOT NULL
+)
 GO
 
 ALTER TABLE [Goals] ADD FOREIGN KEY ([StatusId]) REFERENCES [Status] ([Id])
 GO
 
-ALTER TABLE [Categories] ADD FOREIGN KEY ([Id]) REFERENCES [Skills] ([CategoryId])
+ALTER TABLE [Goals] ADD FOREIGN KEY ([DificultyId]) REFERENCES [Dificulty] ([Id])
 GO
 
 ALTER TABLE [GoalsSkills] ADD FOREIGN KEY ([GoalId]) REFERENCES [Goals] ([Id])
 GO
 
 ALTER TABLE [GoalsSkills] ADD FOREIGN KEY ([SkillId]) REFERENCES [Skills] ([Id])
+GO
+
+ALTER TABLE [Skills] ADD FOREIGN KEY ([CategoryId]) REFERENCES [Categories] ([Id])
+GO
+
+ALTER TABLE [Users] ADD FOREIGN KEY ([GoalId]) REFERENCES [Goals] ([Id])
+GO
+
+ALTER TABLE [UserData] ADD FOREIGN KEY ([Id]) REFERENCES [Users] ([UserDataId])
 GO
