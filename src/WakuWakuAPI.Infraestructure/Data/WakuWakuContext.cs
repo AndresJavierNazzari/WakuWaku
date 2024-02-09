4eea247a39791fb50888588738e29160a4f45c45
Andres Nazzari <andres.nazzari@jala.university>
2024-02-09 08:10:24 -0300

    Add WakuWakuContext class

---
+++ src/WakuWakuAPI.Infraestructure/Data/WakuWakuContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using WakuWakuAPI.Domain.Models;
+
+namespace WakuWakuAPI.Infraestructure.Data;
+
+public class WakuWakuContext : DbContext
+{
+    public WakuWakuContext(DbContextOptions<WakuWakuContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Goal> Goals { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+
+
+
+}
