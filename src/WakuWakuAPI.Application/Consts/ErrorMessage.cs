0a09afc33b4a64edfb4bb05ecdabc94d1487bdeb
Andres Nazzari <andres.nazzari@jala.university>
2024-02-13 07:11:01 -0300

    Add ErrorMessage constants

---
+++ src/WakuWakuAPI.Application/Consts/ErrorMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace WakuWakuAPI.Application.Consts;
+
+public static class ErrorMessage
+{
+    public const string IdZeroOrNegative = "The id cannot be zero or negative.";
+
+    public const string CategoryListEmpty = "Categories list is empty.";
+    public const string CategoryIdNotFound = "Category not found, id: ";
+
+    public const string ResourceNotFound = "Resource was not found";
+    public const string ArgumentNullExceptionMessage = "The argument cannot be null";
+    public const string ArgumentExceptionMessage = "The argument is not valid";
+    public const string InvalidOperationExceptionMessage = "The operation is not valid";
+
+
+}
