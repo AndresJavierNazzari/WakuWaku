d89eb2a059c8ae9fc97075a9430e7054a301d716
Andres Nazzari <andres.nazzari@jala.university>
2024-02-13 07:10:49 -0300

    Refactor EmptyIdException class and method name

--- src/WakuWakuAPI.Application/CrossCutting/EmptyIdException.cs
+++ src/WakuWakuAPI.Application/CrossCutting/EmptyIdException.cs
@@ -1,11 +1,16 @@
 ﻿namespace WakuWakuAPI.Application.CrossCutting;
-public class EmptyIdException : ArgumentException {
-    public EmptyIdException(string message) : base(message) {
+
+public class EmptyIdException : ArgumentException
+{
+    public EmptyIdException(string message) : base(message)
+    {
     }
 
-    public static void ThrowIfEmpty(
-        int id, string? message = null, string? elementName = null) {
-        if(id == 0) {
+    public static void ThrowIfIdZero(
+        int id, string? message = null, string? elementName = null)
+    {
+        if(id == 0)
+        {
             throw new EmptyIdException(message ?? $"{elementName} Id cannot be empty");
         }
     }
