19562f9e4ecd318beab8b0b4c485b6b7784fb827
Andres Nazzari <andres.nazzari@jala.university>
2024-02-13 08:00:13 -0300

    Add Id property to CategoryForUpdate class

--- src/WakuWakuAPI.Domain/DTOs/CategoryForUpdate.cs
+++ src/WakuWakuAPI.Domain/DTOs/CategoryForUpdate.cs
@@ -1,5 +1,6 @@
 ﻿namespace WakuWakuAPI.Domain.DTOs;
 public class CategoryForUpdate {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
 }
