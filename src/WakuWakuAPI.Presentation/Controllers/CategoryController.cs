d4852f3cbbc95fda1e8a372f13a81e7047ac839a
Andres Nazzari <andres.nazzari@jala.university>
2024-02-13 07:11:24 -0300

    Refactor CategoryController to use async/await

--- src/WakuWakuAPI.Presentation/Controllers/CategoryController.cs
+++ src/WakuWakuAPI.Presentation/Controllers/CategoryController.cs
@@ -4,20 +4,22 @@ using FluentValidation;
 using FluentValidation.Results;
 using WakuWakuAPI.Domain.Models;
 using WakuWakuAPI.Domain.DTOs;
-using WakuWakuAPI.Application.Services.Interfaces;
+using WakuWakuAPI.Application.Consts;
 using WakuWakuAPI.Application.CrossCutting;
-
+using WakuWakuAPI.Application.Services.Interfaces;
 
 namespace WakuWakuAPI.Presentation.Controllers;
 
 [ApiController]
-[ApiVersion("1.0")]
+[ApiVersion("1")]
 [Route("api/v{version:apiVersion}/[controller]")]
-public class CategoryController : ControllerBase {
+public class CategoryController : ControllerBase
+{
     private readonly ICategoryService _categoryService;
     private IValidator<CategoryForCreation> _validator;
 
-    public CategoryController(IValidator<CategoryForCreation> validator, ICategoryService categoryService) {
+    public CategoryController(IValidator<CategoryForCreation> validator, ICategoryService categoryService)
+    {
         _validator = validator;
         _categoryService = categoryService;
     }
@@ -26,8 +28,9 @@ public class CategoryController : ControllerBase {
     [HttpGet]
     [HttpHead]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public ActionResult<IEnumerable<Category>> GetCategories([FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] string filter = "") {
-        var categories = _categoryService.GetCategories(page, pageSize, filter);
+    public async Task<ActionResult<IEnumerable<Category>>> GetCategoriesAsync([FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] string filter = "")
+    {
+        var categories = await _categoryService.GetCategoriesAsync(page, pageSize, filter);
         return Ok(categories);
     }
 
@@ -36,22 +39,52 @@ public class CategoryController : ControllerBase {
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<Category> GetCategoryById(int id = 0) {
-        EmptyIdException.ThrowIfEmpty(id);
-        var category = _categoryService.GetCategoryById(id);
-
+    public async Task<ActionResult<Category>> GetCategoryById(int id = 0)
+    {
+        EmptyIdException.ThrowIfIdZero(id, ErrorMessage.IdZeroOrNegative);
+        var category = await _categoryService.GetCategoryByIdAsync(id);
         return Ok(category);
     }
 
+    // DELETE: /Category/{id}
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteCategory(int id = 0)
+    {
+        EmptyIdException.ThrowIfIdZero(id);
+        var deletedCategory = _categoryService.DeleteCategoryByIdAsync(id);
+
+        var response = new
+        {
+            message = $"Resource deleted: category {id}",
+            deletedCategory
+        };
+
+        return Ok(response);
+    }
+
+    // OPTIONS: /Category
+    [HttpOptions]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult OptionsCategory()
+    {
+        Response.Headers.Append("Allow", "GET, POST, PUT, DELETE, OPTIONS");
+        return Ok();
+    }
+    /*
     // POST: /Category
     [HttpPost(Name = "CreateCategory")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
-    public ActionResult<Category> CreateCategory([FromBody] CategoryForCreation categoryForCreation) {
+    public ActionResult<Category> CreateCategory([FromBody] CategoryForCreation categoryForCreation)
+    {
 
         ValidationResult result = _validator.Validate(categoryForCreation);
 
-        if(!result.IsValid) {
+        if(!result.IsValid)
+        {
             throw new ValidationException(result.Errors);
         }
 
@@ -64,37 +97,18 @@ public class CategoryController : ControllerBase {
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<Category> UpdateCategory([FromBody] CategoryForUpdate categoryForUpdate, int id = 0) {
-        EmptyIdException.ThrowIfEmpty(id);
+    public ActionResult<Category> UpdateCategory([FromBody] CategoryForUpdate categoryForUpdate, int id = 0)
+    {
+        EmptyIdException.ThrowIfIdZero(id);
         var category = _categoryService.UpdateCategory(id, categoryForUpdate);
 
         return Ok(category);
     }
 
-    // DELETE: /Category/{id}
-    [HttpDelete("{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult DeleteCategory(int id = 0) {
-        EmptyIdException.ThrowIfEmpty(id);
-        var deletedCategory = _categoryService.DeleteCategoryById(id);
+    
 
-        var response = new {
-            message = $"Resource deleted: category {id}",
-            deletedCategory
-        };
-
-        return Ok(response);
-    }
-
-    // OPTIONS: /Category
-    [HttpOptions]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult OptionsCategory() {
-        Response.Headers.Append("Allow", "GET, POST, PUT, DELETE, OPTIONS");
-        return Ok();
-    }
+   
+    */
 }
 
 
