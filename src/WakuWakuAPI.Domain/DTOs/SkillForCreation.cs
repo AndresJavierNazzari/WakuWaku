66fcdb060f1729448edc17f2dcff51c8a2e86c43
Andres Nazzari <andres.nazzari@jala.university>
2024-02-14 07:13:13 -0300

    Add SkillForCreation and SkillForUpdate DTOs

---
+++ src/WakuWakuAPI.Domain/DTOs/SkillForCreation.cs
@@ -0,0 +1,8 @@
+﻿namespace WakuWakuAPI.Domain.DTOs;
+
+public class SkillForCreation
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int? CategoryId { get; set; }
+}
---
+++ src/WakuWakuAPI.Domain/DTOs/SkillForUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace WakuWakuAPI.Domain.DTOs;
+
+public class SkillForUpdate
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int? CategoryId { get; set; }
+}
