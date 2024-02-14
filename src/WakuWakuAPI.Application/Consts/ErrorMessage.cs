84c229f5691fa7a7a7f3788d8656b2012e3517b9
Andres Nazzari <andres.nazzari@jala.university>
2024-02-14 07:12:59 -0300

    Add SkillIdNotFound error message

--- src/WakuWakuAPI.Application/Consts/ErrorMessage.cs
+++ src/WakuWakuAPI.Application/Consts/ErrorMessage.cs
@@ -7,6 +7,8 @@ public static class ErrorMessage
     public const string CategoryListEmpty = "Categories list is empty.";
     public const string CategoryIdNotFound = "Category not found, id: ";
 
+    public const string SkillIdNotFound = "Skill not found, id: ";
+
     public const string ResourceNotFound = "Resource was not found";
     public const string ArgumentNullExceptionMessage = "The argument cannot be null";
     public const string ArgumentExceptionMessage = "The argument is not valid";
